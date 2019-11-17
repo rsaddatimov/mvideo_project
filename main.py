5f7031aebecc56f05ae833b2cf00f4daf587f677
rsaddatimov <rafa.saddatimov@gmail.com>
2019-11-18 01:35:11 +0300

    changes

--- main.py
+++ main.py
@@ -22,8 +22,7 @@ if args.log:
 else:
     logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s')
 
-logging.info('smth')
-
+logging.info('Preparing...')
 stream = cv2.VideoCapture(args.input)
 
 cv2.namedWindow('Frame', cv2.WINDOW_NORMAL)
@@ -31,15 +30,20 @@ cv2.resizeWindow('Frame', 850, 500)
 
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+frame_id = 1
+
+logging.info('Starting video stream...')
 while True:
     ret, frame = stream.read()
 
-    #frame = imutils.resize(frame, 300, 300)
+    frame = imutils.resize(frame, 800, 600)
 
     rects, weights = hog.detectMultiScale(frame, winStride=(4, 4), padding=(8, 8), scale=1.05)
 
     rects = np.array([[x, y, x + w, y + h] for x, y, w, h in rects])
     rects = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+
+    logging.info('%d-th frame: Detected %d objects...' % (frame_id, len(rects)))
 
     for x, y, xx, yy in rects:
         cv2.rectangle(frame, (x, y), (xx, yy), (0, 0, 255), 2)
@@ -48,6 +52,10 @@ while True:
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+    frame_id += 1
+
+logging.info('Ending stream...')
 
 stream.release()
 cv2.destroyAllWindows()
