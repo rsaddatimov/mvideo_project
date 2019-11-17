28b6b5bb7757cdafaf6b3f878f12ee592605c89d
rsaddatimov <rafa.saddatimov@gmail.com>
2019-11-18 00:44:52 +0300

    features

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import logging
 import argparse
+import numpy as np
+import cv2
+from imutils.object_detection import non_max_suppression
+import imutils
 
 #command line arguments parsing
 
 arg_parser = argparse.ArgumentParser()
+
 arg_parser.add_argument('-i', '--input', help='Path to CCTV cam ip file')
 arg_parser.add_argument('-l', '--log', help='Path to log file')
 
 args = arg_parser.parse_args()
 
-if args.input:
-    pass
-else:
+if not args.input:
     raise Exception('No input file')
 
 if args.log:
@@ -21,3 +24,30 @@ else:
 
 logging.info('smth')
 
+stream = cv2.VideoCapture(args.input)
+
+cv2.namedWindow('Frame', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('Frame', 850, 500)
+
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+while True:
+    ret, frame = stream.read()
+
+    #frame = imutils.resize(frame, 300, 300)
+
+    rects, weights = hog.detectMultiScale(frame, winStride=(4, 4), padding=(8, 8), scale=1.05)
+
+    rects = np.array([[x, y, x + w, y + h] for x, y, w, h in rects])
+    rects = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+
+    for x, y, xx, yy in rects:
+        cv2.rectangle(frame, (x, y), (xx, yy), (0, 0, 255), 2)
+
+    cv2.imshow('Frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+stream.release()
+cv2.destroyAllWindows()
