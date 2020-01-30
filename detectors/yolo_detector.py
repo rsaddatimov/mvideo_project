1b1bea29af0f17a585ebc6d6bca8c04f192353c5
rsaddatimov <rafa.saddatimov@gmail.com>
2020-01-30 06:17:16 +0300

    features

--- detectors/yolo_detector.py
+++ detectors/yolo_detector.py
@@ -10,10 +10,71 @@ from os.path import exists
 """
 class YoloDetector:
 
-    def __init__(self, modelPath):
-        if not exists(modelPath):
+    """
+    Кокструктор класса. Инициализирует всё необходимое
+    @in mdelPath - путь к модели
+    @in confidence - минимально допустимая вероятность обнаружения
+    @in threshold
+    """
+    def __init__(self, modelPath, confidence, threshold):
+        if not exists(pj(modelPath, 'yolo3.cfg')) or not exists(pj(modelPath, 'yolo3.weights')):
             raise Exception('Model was not found at %s' % modelPath)
         self.net = cv2.dnn.readNetFromDarknet(pj(modelPath, 'yolo3.cfg'), pj(modelPath, 'yolo3.weights'))
 
+        self.layersNames = self.net.getLayerNames()
+        self.layersNames = [self.layersNames[id[0] - 1] for id in self.net.getUnconnectedOutLayers()]
+
+        self.acceptableConfidence = confidence
+        self.threshold = threshold
+
+        self.backScaleArray = None
+
+    """
+    Метод, который по кадру дает детекты на ней в виде массива прямоугольников
+    @in inputFrame - подаваемый на обработку кадр
+    @return - лист из прямоугольников
+    """
     def detect(self, inputFrame):
+        if self.backScaleArray:
+            (H, W) = inputFrame.shape[:2]
+            self.backScaleArray = np.array([W, H, W, H])
+
+        # Скармливаем нейронке кадр
         blob = cv2.dnn.blobFromImage(inputFrame, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+        self.net.setInput(blob)
+        outputLayers = self.net.forward(self.layersNames)
+
+        resultingRectangles = []
+        resultingConfidences = []
+
+        # Итерируемся по обнаружениям
+        for layer in outputLayers:
+            for detection in layer:
+                probabilities = detection[5:]
+                classId = np.argmax(probabilities)
+                confidence = probabilities[classId]
+
+                # Проверяем подходит ли нам детект
+                # В данном случае classId должен быть равен 0, так как 0 соответствует человеку
+                if classId or confidence <= self.acceptableConfidence:
+                    continue
+
+                # Строим прямоугольник и добавляем его к обнаружениям
+                rect = detection[0:4] * self.backScaleArray
+                (centerX, centerY, width, height) = rect.astype("int")
+                cornerX = int(centerX - (width / 2))
+                cornerY = int(centerY - (height / 2))
+
+                resultingRectangles.append([cornerX, cornerY, int(width), int(height)])
+                resultingConfidences.append(confidence)
+
+                # Не забываем применить non-maximum suppression чтобы
+                # избавиться от неоправданных пересечений прямоугоьлников
+                resultingRectangles = cv2.dnn.NMSBoxes(
+                    resultingRectangles,
+                    resultingConfidences,
+                    self.acceptableConfidence,
+                    self.threshold
+                )
+
+                return resultingRectangles, resultingConfidences
