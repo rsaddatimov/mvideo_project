f82804696ae122da736bdf315014389a6cff22cb
rsaddatimov <rafa.saddatimov@gmail.com>
2020-01-31 04:30:47 +0300

    features

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import argparse
 import cv2
 import numpy as np
 
-#command line arguments parsing
+from datetime import *
+from detectors import YoloDetector
+from ioapi.multicam import RemoteMultiCapture
+from os import getenv
+from urllib.parse import quote_plus
 
+# Обрабатываем аргументы командной строки
 argParser = argparse.ArgumentParser()
 
 argParser.add_argument('--channel', required=True, type=int, help='The CCTV channel to listen')
@@ -17,4 +22,22 @@ argParser.add_argument('--polygon-path', required=True, type=str, help='Path to 
 argParser.add_argument('--start-time', required=True, type=str, help='Timestamp from which we listen channel')
 
 argv = argParser.parse_args()
+
+CCTV_TOKEN = getenv('TOKEN', 'secret-key')
+CCTV_IP = getenv('IP', 'localhost')
+
+# Инициализируем видеопоток
+remoteCapture = RemoteMultiCapture('ws://' + CCTV_IP + '/api?token=' + quote_plus(CCTV_TOKEN))
+remoteCapture.channels = [argv.channel]
+remoteCapture.fpsx = argv.fps_multiplier
+remoteCapture.pos = datetime.strptime(START, '%d.%m.%Y %H:%M:%S')
+
+# Инициализируем детектор
+detector = YoloDetector(
+    argv.model_path,
+    argv.min_confidence,
+    argv.nms_threshold,
+    argv.gpu_enabled
+)
+
 
